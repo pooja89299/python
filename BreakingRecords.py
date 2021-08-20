f9d3228a067c245611e4841e5e081833418b6458
pooja89299 <82705821+pooja89299@users.noreply.github.com>
2021-08-20 21:48:20 +0530

    Add files via upload

---
+++ BreakingRecords.py
@@ -0,0 +1,12 @@
+a=input("enter a games")
+a=list(map(int,input().split()))
+m=M=a[0]
+x=y=0
+for i in a[1:]:
+    if i>M:
+        M=i
+        x+=1
+    elif i<m:
+        m=i
+        y+=1
+print(x,y)
